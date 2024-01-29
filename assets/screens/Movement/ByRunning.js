99b9cce9ba24888db94a985ecef69aa1eda2a31d
OoChaPtpn <138839110+OoChaPtpn@users.noreply.github.com>
2024-01-29 19:26:16 +0700

    ByRunning_Screen     - Dropdown

--- assets/screens/Movement/ByRunning.js
+++ assets/screens/Movement/ByRunning.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Text, View, ScrollView, TextInput } from 'react-native'
 // import { TextInput } from 'react-native-paper';
 import styles from '../../styles/StyleSheets'
 import { COLORS, FONTS } from '../../constants'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Dropdown } from 'react-native-element-dropdown';
 
+const data = [
+    { label: 'Item 1', value: '1' },
+    { label: 'Item 2', value: '2' },
+    { label: 'Item 3', value: '3' },
+    { label: 'Item 4', value: '4' },
+    { label: 'Item 5', value: '5' },
+    { label: 'Item 6', value: '6' },
+    { label: 'Item 7', value: '7' },
+    { label: 'Item 8', value: '8' },
+  ];
 
 const ByRunning_Screen = () => {
-    const [number, onChangeNumber] = React.useState('');
-    const [age, setAge] = React.useState('');
+    const [number, onChangeNumber] = useState('');
+    const [age, setAge] = useState('');
+    const [value, setValue] = useState(null);
 
     const handleChange = (event) => {
         setAge(event.target.value);
     };
+
 
     return (
         <View style={styles.container}>
@@ -54,7 +67,30 @@ const ByRunning_Screen = () => {
                         style={[, { ...FONTS.body3, color: COLORS.dark, width: '100%' }]}
                         onChangeText={onChangeNumber}
                         value={number}
-                        keyboardType="text"
+                        keyboardType="default"
+                    />
+                </View>
+                <View>
+                    <Dropdown
+                        style={[styles.txInput, { ...FONTS.body3, color: COLORS.dark }]}
+                        placeholderStyle={styles.placeholderStyle}
+                        selectedTextStyle={styles.selectedTextStyle}
+                        inputSearchStyle={styles.inputSearchStyle}
+                        iconStyle={styles.iconStyle}
+                        data={data}
+                        search
+                        maxHeight={300}
+                        labelField="label"
+                        valueField="value"
+                        placeholder="Select item"
+                        searchPlaceholder="Search..."
+                        value={value}
+                        onChange={item => {
+                        setValue(item.value);
+                        }}
+                        renderLeftIcon={() => (
+                        <Icon style={styles.icon} color="black" name="account-search-outline" size={20} />
+                        )}
                     />
                 </View>
 
